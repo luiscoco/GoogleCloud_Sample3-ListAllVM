756ebe2cb5f8ab8654d1706a2538197d08a362d2
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-06-15 11:38:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Google.Cloud.Compute.V1;
+
+string projectId = "focus-cache-387205";
+// Initialize client that will be used to send requests. This client only needs to be created
+// once, and can be reused for multiple requests.
+InstancesClient client = await InstancesClient.CreateAsync();
+IList<Instance> allInstances = new List<Instance>();
+
+// Make the request to list all VM instances in a project.
+await foreach (var instancesByZone in client.AggregatedListAsync(projectId))
+{
+    // The result contains a KeyValuePair collection, where the key is a zone and the value
+    // is a collection of instances in that zone.
+    Console.WriteLine($"Instances for zone: {instancesByZone.Key}");
+    foreach (var instance in instancesByZone.Value.Instances)
+    {
+        Console.WriteLine($"-- Name: {instance.Name}");
+        allInstances.Add(instance);
+    }
+}
